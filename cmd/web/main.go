aa3b1f16aafd6845714ea4459062a71492df6fdd
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-08-13 18:39:11 +0300

    problem with 4000

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 /*
@@ -12,17 +15,59 @@ import (
 	и созданный роутер
 */
 func main() {
-	mux := http.NewServeMux() // новый роутер
-	mux.HandleFunc("/", home) // регистрирует функцию home как обработчик для роутера mux ...
+	addr := flag.String("addr", "127.0.0.1:4000", "Сетевоой адресс HTTP")         // флаг командной строки
+	flag.Parse()                                                                  // извлечение флага из командной строки(меняет по адресу addr)
+	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  // создание логгера INFO в stdout
+	errorlog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // логгер ошибок ERROR
+	mux := http.NewServeMux()                                                     // новый роутер
+	mux.HandleFunc("/", home)                                                     // регистрирует функцию home как обработчик для роутера mux ...
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// инициализация FileServer, который будет обрабатывать HTTP-запросы к статическим файлам из ./ui/static/
-	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+	fileServer := http.FileServer(neuterdFileSystem{http.Dir("./static")})
 	// регистрация всех запросов начинающихся со "/static"
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux) // Запуск нового веб-сервера
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorlog,
+		Handler:  mux,
+	}
+	infolog.Printf("Запуск веб-сервера на http://%s", *addr)
+	err := srv.ListenAndServe() // Запуск нового веб-сервера
+	errorlog.Fatal(err)
+}
+
+/*
+ Создаем настраиваемый тип, который включает в себя http.FileSystem
+*/
+type neuterdFileSystem struct {
+	fs http.FileSystem
+}
+
+/*
+	Open():
+	Создаем метод для neuteredFileSystem, который вызывается каждый раз, когда http.FileServer получает запрос
+	Возвращает файл, усли он не существует вернет ошибку os.ErrNotExist (преобразуется в 404)
+	Также закрывает файл
+*/
+func (new_fs neuterdFileSystem) Open(path string) (http.File, error) {
+	f, err := new_fs.fs.Open(path) // открываем вызываемый путь
+	if err != nil {
+		return nil, err
+	}
+	s, err := f.Stat() // os.File предоставляет доступ к информации о файле/пути os.FileInfo
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := new_fs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
