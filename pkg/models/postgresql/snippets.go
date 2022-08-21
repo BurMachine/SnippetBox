ed015fcf481450e7bc06d908a069f0f1f74d9b1d
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-08-21 20:38:35 +0300

    postgres error

--- pkg/models/postgresql/snippets.go
+++ pkg/models/postgresql/snippets.go
@@ -16,8 +16,15 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	// Ниже будет SQL запрос, который мы хотим выполнить. Мы разделили его на две строки
 	// для удобства чтения (поэтому он окружен обратными кавычками
 	// вместо обычных двойных кавычек).
-	stmt := `INSERT INTO snippets (title, content, created, expires)
-    VALUES(?, ?, current_timestamp, current_timestamp + interval '1 year'))`
+	//str := []rune(expires) + 'year'
+	//str = append(str, 'year')
+
+	//stmt := fmt.Sprintf("INSERT INTO snippets (title, content_, created, expires)VALUES(%s, %s,  current_timestamp, current_timestamp + interval '%s years", title, content, expires)
+
+	//stmt := `INSERT INTO snippets (title, content_, created, expires)
+	//VALUES($1, $2, current_timestamp, current_timestamp + $3::interval)`
+	stmt := `INSERT INTO snippets (title, content_, created, expires)
+		VALUES($1, $2, current_timestamp, current_timestamp + interval '1 year' * $3)`
 	_, err := m.DB.Exec(stmt, title, content, expires) // много вопросов
 	if err != nil {
 		return 0, err
